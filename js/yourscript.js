9fa2b3089b92cc81ad6bb15cc4255672657ed97e
rayaankhan <khan.rayaan2003@gmail.com>
2024-03-07 01:23:23 +0530

    Commented unnecessary code

--- js/yourscript.js
+++ js/yourscript.js
@@ -45,19 +45,20 @@ const createArrow = (point, nextPoint) => {
 };
 
 // Circle and arrow creation module
-const createCirclesAndArrows = (map, pointsData) => {
-  const arrows = [];
+// const createCirclesAndArrows = (map, pointsData) => {
+//   const arrows = [];
 
-  for (let i = 0; i < pointsData.length; i++) {
-    const point = pointsData[i];
-    const nextPoint = pointsData[(i + 1) % pointsData.length];
-    const circle = createCircle(point);
-    const arrow = createArrow(point, nextPoint);
-    arrows.push(arrow);
+//   for (let i = 0; i < pointsData.length; i++) {
+//     const point = pointsData[i];
+//     const nextPoint = pointsData[(i + 1) % pointsData.length];
+//     const circle = createCircle(point);
+//     const arrow = createArrow(point, nextPoint);
+//     arrows.push(arrow);
 
-    const container = createPopupContainer(point, i, pointsData, arrows, map);
-    const popup = circle.bindPopup(container, { className: "custom-popup" });
+//     const container = createPopupContainer(point, i, pointsData, arrows, map);
+//     const popup = circle.bindPopup(container, { className: "custom-popup" });
 
+<<<<<<< Updated upstream
     circle.on("click", () => {
       toggleArrows(i, arrows);
       openPopupWithButtons(point, i, pointsData, arrows, map);
@@ -66,44 +67,52 @@ const createCirclesAndArrows = (map, pointsData) => {
 
   return arrows;
 };
+=======
+//     circle.on("click", () => {
+//       toggleArrows(i, arrows);
+//       openPopupWithButtons(point, i, pointsData, arrows, map);
+//     });
+//   }
+// };
+>>>>>>> Stashed changes
 
-// Popup container creation module
-const createPopupContainer = (point, index, pointsData, arrows, map) => {
-  const container = L.DomUtil.create("div", "popup-container");
-  const contentDiv = L.DomUtil.create("div", "popup-content", container);
-  contentDiv.innerHTML = point.content;
+// // Popup container creation module
+// const createPopupContainer = (point, index, pointsData, arrows, map) => {
+//   const container = L.DomUtil.create("div", "popup-container");
+//   const contentDiv = L.DomUtil.create("div", "popup-content", container);
+//   contentDiv.innerHTML = point.content;
 
-  const nextButton = createPopupButton("Next", () => {
-    const nextIndex = (index + 1) % pointsData.length;
-    // map.closePopup();
-    toggleArrows(nextIndex, arrows);
-    openPopupWithButtons(
-      pointsData[nextIndex],
-      nextIndex,
-      pointsData,
-      arrows,
-      map
-    );
-  });
+//   const nextButton = createPopupButton("Next", () => {
+//     const nextIndex = (index + 1) % pointsData.length;
+//     // map.closePopup();
+//     toggleArrows(nextIndex, arrows);
+//     openPopupWithButtons(
+//       pointsData[nextIndex],
+//       nextIndex,
+//       pointsData,
+//       arrows,
+//       map
+//     );
+//   });
 
-  const prevButton = createPopupButton("Previous", () => {
-    const prevIndex = (index - 1 + pointsData.length) % pointsData.length;
-    // map.closePopup();
-    toggleArrows(prevIndex, arrows);
-    openPopupWithButtons(
-      pointsData[prevIndex],
-      prevIndex,
-      pointsData,
-      arrows,
-      map
-    );
-  });
+//   const prevButton = createPopupButton("Previous", () => {
+//     const prevIndex = (index - 1 + pointsData.length) % pointsData.length;
+//     // map.closePopup();
+//     toggleArrows(prevIndex, arrows);
+//     openPopupWithButtons(
+//       pointsData[prevIndex],
+//       prevIndex,
+//       pointsData,
+//       arrows,
+//       map
+//     );
+//   });
 
-  container.appendChild(nextButton);
-  container.appendChild(prevButton);
+//   container.appendChild(nextButton);
+//   container.appendChild(prevButton);
 
-  return container;
-};
+//   return container;
+// };
 
 // Popup button creation module
 const createPopupButton = (label, onClick) => {
@@ -114,14 +123,14 @@ const createPopupButton = (label, onClick) => {
 };
 
 // Popup with buttons module
-const openPopupWithButtons = (point, index, pointsData, arrows, map) => {
-  const container = createPopupContainer(point, index, pointsData, arrows, map);
+// const openPopupWithButtons = (point, index, pointsData, arrows, map) => {
+//   const container = createPopupContainer(point, index, pointsData, arrows, map);
 
-  const popup = L.popup()
-    .setLatLng([point.lat, point.lon])
-    .setContent(container)
-    .openOn(map);
-};
+//   const popup = L.popup()
+//     .setLatLng([point.lat, point.lon])
+//     .setContent(container)
+//     .openOn(map);
+// };
 
 // Arrows visibility toggle module
 const toggleArrows = (selectedIndex, arrows) => {
